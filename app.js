389bad43d0be91947883ed18dc537cbefb98b74c
Colin Ferguson <colinferguson@trentu.ca>
2016-12-15 02:53:53 -0500

    Blablabla

--- app.js
+++ app.js
@@ -17,11 +17,13 @@ function($scope){
       $scope.posts.push({
         title: $scope.title,
         link: $scope.link,
-        upvotes: 0})
+        upvotes: 0
+      })
 
       $scope.title = '';
       $scope.link = '';
     };
+
     $scope.incrementUpvotes = function(post){
       post.upvotes += 1;
     }
