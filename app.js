e5fcf7a56ee907b846133a7934dd7f4d01824e13
Colin Ferguson <colinferguson@trentu.ca>
2016-12-15 03:24:41 -0500

    Blergha

--- app.js
+++ app.js
@@ -1,15 +1,32 @@
-var app = angular.module('Reddit Clone', []);
+var app = angular.module('Reddit Clone', [ui.router]);
+
+
+app.config([
+  '$stateProvider',
+  '$urlRouterProvider',
+  function($stateProvider, $urlRouterProvider) {
+    $stateProvider
+    .state('home', {
+      url: '/home',
+      templateUrl: '/home.html',
+      controller: 'MainCtrl'
+    });
+
+  $urlRouterProvider.otherwise('home');
+}]);
+
+app.factory('posts', [function(){
+  var o = {
+    posts: []
+  };
+  return o;
+}]);
 
 app.controller('MainCtrl', [
 '$scope',
-function($scope){
-  $scope.posts = [
-    {title: 'post 1', upvotes: 5},
-    {title: 'post 2', upvotes: 2},
-    {title: 'post 3', upvotes: 15},
-    {title: 'post 4', upvotes: 9},
-    {title: 'post 5', upvotes: 4}
-  ];
+'posts',
+function($scope, posts){
+  $scope.posts = posts.posts;
 
     $scope.addPost = function() {
       if(!$scope.title || $scope.title === '') { return; }
